2777e965e61d5b6689fc33caba588f867f9b6222
binghui2 <chengbinghui19990525@gmail.com>
2018-09-27 21:02:06 -0500

    "physics features added"

--- core/src/com/recursive/corruption/gravoball/Chaser.java
+++ core/src/com/recursive/corruption/gravoball/Chaser.java
@@ -4,27 +4,37 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 public class Chaser extends Ball implements Physics{
     Chaser(float x, float y) {
-        super(x, y, 6.f,10.f , new Color(.2f, .5f, 1.0f, 1.0f));
+        super(x, y, 6.f,100.f , new Color(.2f, .5f, 1.0f, 1.0f));
     }
 
 
-        private float Duration = 0;
+        private float duration = 0;
     public void update(GravoBallGame game,float chaserDt) {
         Player player = game.getPlayer();
-        float dx = player.x - x;
+       /* float dx = player.x - x;
         float dy = player.y - y;
         double dist = Math.sqrt(dx * dx + dy * dy);
         x += dx / dist;
-        y += dy / dist;
+        y += dy / dist;*/
 
-        Duration = chaserDt;
+        duration = chaserDt;
+        Vector2 forceFromPlayer = gravityforce(player);
+        this.changePosition(forceFromPlayer);
+        this.changeVelocity(forceFromPlayer);
     }
 
+    public void changePosition(Vector2 force) {
+        Vector2 acceleration = this.getAccelaration(force);
+        this.x += this.velocity.x* duration;
+        this.y += this.velocity.y* duration;
+    }
     @Override
     public void changeVelocity(Vector2 force ) {
         Vector2 acceleration = this.getAccelaration(force);
-        this.velocity.x += acceleration.x*Duration;
-        this.velocity.y += acceleration.y*Duration;
+        this.velocity.x += acceleration.x* duration;
+        this.velocity.y += acceleration.y* duration;
+        this.velocity.x = this.velocity.x*0.99f;
+        this.velocity.y = this.velocity.y*0.99f;
 
     }
 
@@ -39,8 +49,12 @@ public class Chaser extends Ball implements Physics{
 
     @Override
     public Vector2 gravityforce(Ball b) {
-        float forceInX = playableConstant*b.mass*this.mass/((b.x-this.x)*(b.x-this.x));
-        float forceInY = playableConstant*b.mass*this.mass/((b.y-this.y)*(b.y-this.y));
+        float directionInX = Math.abs(b.x-this.x)/(b.x-this.x);
+        float directionInY = Math.abs(b.y-this.y)/(b.y-this.y);
+         float r =  (float)(Math.sqrt(((b.x-this.x)*(b.x-this.x) + (b.y-this.y)*(b.y-this.y))));
+        float totalForce = gravity *b.mass*this.mass/r*r;
+       float forceInX = totalForce*(b.x-this.x)/r;
+        float forceInY = totalForce*(b.y-this.y)/r;
         Vector2 res =  new Vector2(forceInX,forceInY);
         return res;
     }
--- core/src/com/recursive/corruption/gravoball/Player.java
+++ core/src/com/recursive/corruption/gravoball/Player.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.math.Vector2;
 public class Player extends Ball implements Physics {
     private float playerMass = 90;
     Player(float x, float y) {
-        super(x, y, 6.f, 90 ,new Color(1.0f, .1f, .1f, 1.0f));
+        super(x, y, 6.f, 9000.f ,new Color(1.0f, .1f, .1f, 1.0f));
     }
 
     private float Duration = 0;
@@ -20,16 +20,25 @@ public class Player extends Ball implements Physics {
     @Override
 
     public Vector2 gravityforce(Ball b) {
-        return null;
+        float forceInX = gravity *b.mass*this.mass/((b.x-this.x)*(b.x-this.x));
+        float forceInY = gravity *b.mass*this.mass/((b.y-this.y)*(b.y-this.y));
+        Vector2 res =  new Vector2(forceInX,forceInY);
+        return res;
     }
 
     @Override
     public Vector2 getAccelaration(Vector2 force) {
-        return null;
+        float xAcceleration = force.x/mass;
+        float yAcceleration = force.y/mass;
+        Vector2 acceleration = new Vector2(xAcceleration,yAcceleration);
+        return acceleration;
     }
 
     @Override
     public void changeVelocity(Vector2 force) {
+        Vector2 acceleration = this.getAccelaration(force);
+        this.velocity.x += acceleration.x*Duration;
+        this.velocity.y += acceleration.y*Duration;
 
     }
 }
