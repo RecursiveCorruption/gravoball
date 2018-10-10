d5117331e9f62fa1ce620be3fc8f33c9fef7fb73
Matthew Scholefield <matthew331199@gmail.com>
2018-10-09 19:21:55 -0500

    Formatting and simple cleanup  - Remove extraneous blank lines  - Remove leftover code in comments  - replace some single-use variables with direct return statements

--- core/src/com/recursive/corruption/gravoball/Physics.java
+++ core/src/com/recursive/corruption/gravoball/Physics.java
@@ -1,9 +1,10 @@
 package com.recursive.corruption.gravoball;
-import com.badlogic.gdx.math.Vector2;
-interface Physics {
-   final float gravity = 0.098f; // Set to a number to make playable
-    Vector2 gravityforce (Ball b);
-    Vector2 getAccelaration(Vector2 force) ;
-    void changeVelocity(Vector2 force);
 
+import com.badlogic.gdx.math.Vector2;
+
+interface Physics {
+    float gravity = 0.098f; // Set to a number to make playable
+    Vector2 gravityforce(Ball b);
+    Vector2 getAccelaration(Vector2 force);
+    void changeVelocity(Vector2 force);
 }
--- core/src/com/recursive/corruption/gravoball/Player.java
+++ core/src/com/recursive/corruption/gravoball/Player.java
@@ -5,13 +5,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player extends Ball implements Physics {
-    private float playerMass = 90;
     Player(float x, float y) {
-        super(x, y, 6.f, 9000.f ,new Color(1.0f, .1f, .1f, 1.0f));
+        super(x, y, 6.f, 9000.f, new Color(1.0f, .1f, .1f, 1.0f));
     }
 
     private float Duration = 0;
-    public void update(GravoBallGame game,float playerDt) {
+
+    public void update(GravoBallGame game, float playerDt) {
         x = Gdx.input.getX();
         y = Gdx.graphics.getHeight() - Gdx.input.getY();
         Duration = playerDt;
@@ -20,25 +20,22 @@ public class Player extends Ball implements Physics {
     @Override
 
     public Vector2 gravityforce(Ball b) {
-        float forceInX = gravity *b.mass*this.mass/((b.x-this.x)*(b.x-this.x));
-        float forceInY = gravity *b.mass*this.mass/((b.y-this.y)*(b.y-this.y));
-        Vector2 res =  new Vector2(forceInX,forceInY);
-        return res;
+        float forceInX = gravity * b.mass * mass / ((b.x - x) * (b.x - x));
+        float forceInY = gravity * b.mass * mass / ((b.y - y) * (b.y - y));
+        return new Vector2(forceInX, forceInY);
     }
 
     @Override
     public Vector2 getAccelaration(Vector2 force) {
-        float xAcceleration = force.x/mass;
-        float yAcceleration = force.y/mass;
-        Vector2 acceleration = new Vector2(xAcceleration,yAcceleration);
-        return acceleration;
+        float xAcceleration = force.x / mass;
+        float yAcceleration = force.y / mass;
+        return new Vector2(xAcceleration, yAcceleration);
     }
 
     @Override
     public void changeVelocity(Vector2 force) {
-        Vector2 acceleration = this.getAccelaration(force);
-        this.velocity.x += acceleration.x*Duration;
-        this.velocity.y += acceleration.y*Duration;
-
+        Vector2 acceleration = getAccelaration(force);
+        velocity.x += acceleration.x * Duration;
+        velocity.y += acceleration.y * Duration;
     }
 }
