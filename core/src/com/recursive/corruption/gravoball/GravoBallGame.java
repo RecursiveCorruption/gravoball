7bcb84cb311ade1020dc3c8b254b5b7a37d5b70c
Matthew Scholefield <matthew331199@gmail.com>
2018-10-09 21:14:09 -0500

    Refactor GravoBallGame

--- core/src/com/recursive/corruption/gravoball/GravoBallGame.java
+++ core/src/com/recursive/corruption/gravoball/GravoBallGame.java
@@ -6,8 +6,6 @@ import com.badlogic.gdx.graphics.GL20;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
 
 public class GravoBallGame extends ApplicationAdapter {
@@ -16,19 +14,9 @@ public class GravoBallGame extends ApplicationAdapter {
     private ArrayList<Entity> entitiesToRemove = new ArrayList<>();
     private Player player;
     private Random random;
+    private float timeTillSpawn = 0f;
 
-    private Timer timer = new Timer();
-    private static final long PERIOD = 1000L;
-    private static final long DELAY = 1000L;
-
-    private TimerTask repeatedTask = new TimerTask() {
-        public void run() {
-            if (entities.size() < 50) {
-                entities.add(new Comet(random.nextFloat() * Gdx.graphics.getWidth(), random.nextFloat() * Gdx.graphics.getHeight()));
-            }
-        }
-    };
-
+    private static final float SPAWN_PERIOD = 0.1f; // in seconds
 
     Player getPlayer() {
         return player;
@@ -40,29 +28,39 @@ public class GravoBallGame extends ApplicationAdapter {
         random = new Random();
         player = new Player(random.nextFloat() * Gdx.graphics.getWidth(), random.nextFloat() * Gdx.graphics.getHeight());
         entities.add(player);
-        timer.scheduleAtFixedRate(repeatedTask, PERIOD, DELAY);
-        entitiesToRemove = new ArrayList<Entity>();
+        entitiesToRemove = new ArrayList<>();
+        timeTillSpawn = 0f;
     }
 
     public void removeEntity(Entity ball) {
         entitiesToRemove.add(ball);
     }
 
-    private void update() {
-        if (entities.size() < 45) {
-            entities.add(new Chaser(random.nextFloat() * Gdx.graphics.getWidth(), random.nextFloat() * Gdx.graphics.getHeight()));
-        }
+    private void update(float dt) {
+        updateSpawner(dt);
 
         for (Entity entity : entities) {
-            entity.update(this, Gdx.graphics.getDeltaTime());
+            entity.update(this, dt);
         }
         entities.removeAll(entitiesToRemove);
         entitiesToRemove.clear();
     }
 
+    private void updateSpawner(float dt) {
+        timeTillSpawn -= dt;
+        if (timeTillSpawn < 0f && entities.size() < 45) {
+            timeTillSpawn = SPAWN_PERIOD;
+            if (random.nextFloat() < 0.85) {
+                entities.add(new Chaser(random.nextFloat() * Gdx.graphics.getWidth(), random.nextFloat() * Gdx.graphics.getHeight()));
+            } else {
+                entities.add(new Comet(random.nextFloat() * Gdx.graphics.getWidth(), random.nextFloat() * Gdx.graphics.getHeight()));
+            }
+        }
+    }
+
     @Override
     public void render() {
-        update();
+        update(Gdx.graphics.getDeltaTime());
 
         Gdx.gl.glClearColor(0.8f, 0.8f, 1.0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
